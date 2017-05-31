da12768b625f26a689221ec10e5104044ded823e
winargo <rwinargo97@gmail.com>
2017-06-01 00:38:37 +0700

    package.json.lock

--- presentation-hello-world/README.md
+++ presentation-hello-world/README.md
@@ -1,4 +1,4 @@
-# Utility
+# utility
 > A [Bespoke.js](http://markdalgleish.com/projects/bespoke.js) presentation, built with [generator-bespoke](https://github.com/markdalgleish/generator-bespoke)
 
 ## View slides locally
--- presentation-hello-world/src/scripts/main.js
+++ presentation-hello-world/src/scripts/main.js
@@ -3,24 +3,24 @@ var bespoke = require('bespoke'),
   cube = require('bespoke-theme-cube'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
-  pdf = require('bespoke-pdf'),
   bullets = require('bespoke-bullets'),
   backdrop = require('bespoke-backdrop'),
   scale = require('bespoke-scale'),
   hash = require('bespoke-hash'),
-  progress = require('bespoke-progress');
+  progress = require('bespoke-progress'),
+  forms = require('bespoke-forms');
 
 // Bespoke.js
 bespoke.from('article', [
   cube(),
   keys(),
   touch(),
-  pdf(),
   bullets('li, .bullet'),
   backdrop(),
   scale(),
   hash(),
-  progress()
+  progress(),
+  forms()
 ]);
 
 // Prism syntax highlighting
