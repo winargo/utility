3b85b7aebf86b6225c69606e717cf78b7058f37d
DenisSuryanto <denis.suryanto.111@gmail.com>
2017-06-05 23:35:50 +0700

    update

--- dist/build/denis.js
+++ dist/build/denis.js
@@ -1,1 +1,14 @@
-//alert("This Is Utility"); //test
+function toggleFullScreen() {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);
+  }
+}
