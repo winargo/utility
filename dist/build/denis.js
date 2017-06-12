339469d765a752e6ec5e97e26499c2a52bdb656d
DenisSuryanto <denis.suryanto.111@gmail.com>
2017-06-13 00:05:20 +0700

    update

--- dist/build/denis.js
+++ dist/build/denis.js
@@ -1,29 +1,33 @@
 function toggleFullScreen() {
-  var doc = window.document;
-  var docEl = doc.documentElement;
-
-  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-
-  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-    requestFullScreen.call(docEl);
-    document.requestFullScreen(docEl);
-       
-  document.body.style.backgroundColor = "#eaeaea" ;
-  }
-  else {
-    cancelFullScreen.call(doc);
-  }
+  
+    "use strict";
+  
+    var doc = window.document,
+        docEl = doc.documentElement,
+        requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen,
+        cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+    if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+        requestFullScreen.call(docEl);
+        document.requestFullScreen(docEl);
+        document.body.style.backgroundColor = "#eaeaea";
+    } else {
+        cancelFullScreen.call(doc);
+    }
 }
 
-function ubah_warna()
-{
+function ubah_warna() {
+    "use strict";
     var warna = document.getElementById("color1").value;
-    document.getElementById("ww").style.color = warna;
+    document.getElementsByTagName("h1").style.color = warna;
+    document.getElementsByTagName("h2").style.color = warna;
+    document.getElementsByTagName("h3").style.color = warna;
+    document.getElementsByTagName("h4").style.color = warna;
+    document.getElementsByTagName("h5").style.color = warna;
+    document.getElementsByTagName("h6").style.color = warna;
+    document.getElementsByTagName("p").style.color = warna;
 }
-
-var bespoke = require('bespoke'),
-    pdf = require('bespoke-pdf');
-
-bespoke.from('articel',[pdf()]);
-
+function ubah_back() {
+    "use strict";
+    var warna1 = document.getElementById("color2").value;
+    document.bgColor = warna1;
+}
