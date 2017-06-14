b838da29cd4a16ab22bf77db662a3a0684ab69ce
winargo <rwinargo97@gmail.com>
2017-06-14 22:37:18 +0700

    test

--- dist/build/jilbert.js
+++
@@ -1,5 +0,0 @@
-function promptpopup(){
-    var st = prompt("");
-    alert(st);
-    
-}
--- dist/build/riandy.js
+++ dist/build/riandy.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-=======
+
 function changeBackground() {
     var color = document.getElementById("color").value; // cached
 
@@ -22,26 +21,5 @@ function scroll(){
 }
 
 
-$("enablemouse").addEventListener("click", function(){
-           document.getElementById("test").innerHTML = "Hello World";
-           })
-var lastScrollTop = 0;
-
-
-1
-down vote
-
-var updated=0,st;
-$('body').on({
-    'touchmove': function(e) { 
-    st = $(this).scrollTop();
-    if(st > updated) {
-        console.log('down');
-    }
-    else {
-        console.log('up');
-    }
-    updated = st;
-    }
-});
->>>>>>> 6695af908aa6a5437e6b09b878d9022f0787ef9b
+document.getElementById("enablemouse").addEventListener("click", function(){
+            document.getElementById("test").innerHTML = "Hello World";})
