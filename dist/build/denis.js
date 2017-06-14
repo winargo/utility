1aeb11539967a023e333fbaac2caf50d76102b1d
winargo <rwinargo97@gmail.com>
2017-06-15 00:43:58 +0700

    Update
    fix moderate bugs

--- dist/build/denis.js
+++ dist/build/denis.js
@@ -1,11 +1,5 @@
 function toggleFullScreen() {
-  
-    "use strict";
-  
-    var doc = window.document,
-        docEl = doc.documentElement,
-        requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen,
-        cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+    var doc = window.document,docEl = doc.documentElement,requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen,cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
     if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
         requestFullScreen.call(docEl);
         document.requestFullScreen(docEl);
@@ -13,22 +7,4 @@ function toggleFullScreen() {
     } else {
         cancelFullScreen.call(doc);
     }
-}
-
-function ubah_warna() {
-    "use strict";
-    var warna = document.getElementById("color1").value;
-    document.getElementsByTagName("h1").style.color = warna;
-    document.getElementsByTagName("h2").style.color = warna;
-    document.getElementsByTagName("h3").style.color = warna;
-    document.getElementsByTagName("h4").style.color = warna;
-    document.getElementsByTagName("h5").style.color = warna;
-    document.getElementsByTagName("h6").style.color = warna;
-    document.getElementsByTagName("p").style.color = warna;
-    document.getElementById("ww").style.color = warna;
-}
-function ubah_back() {
-    "use strict";
-    var warna1 = document.getElementById("color2").value;
-    document.bgColor = warna1;
 }
--- dist/build/riandy.js
+++ dist/build/riandy.js
@@ -1,15 +1,37 @@
-
 function changeBackground() {
-    var color = document.getElementById("color").value; // cached
-
-    // The working function for changing background color.
+    var color = document.getElementById("color1").value;
+    
     document.bgColor = color;
-
-    // The code I'd like to use for changing the text simultaneously - however it does not work.
-    document.getElementById("coltext").style.color = color;
 }
 
-document.getElementById("submitColor").addEventListener("click", changeBackground, false);
+document.getElementById("submitColor1").addEventListener("click", changeBackground, false);
+
+function changeBackground1() {
+    var warna = document.getElementById("color2").value;
+    // The code I'd like to use for changing the text simultaneously - however it does not work.
+    document.getElementById("a1").style.color = warna;
+    document.getElementById("a2").style.color = warna;
+    document.getElementById("a3").style.color = warna;
+    document.getElementById("a4").style.color = warna;
+    document.getElementById("a5").style.color = warna;
+    document.getElementById("a6").style.color = warna;
+    document.getElementById("a7").style.color = warna;
+    document.getElementById("a8").style.color = warna;
+    document.getElementById("a9").style.color = warna;
+    document.getElementById("a10").style.color = warna;
+    document.getElementById("a11").style.color = warna;
+    document.getElementById("a12").style.color = warna;
+    document.getElementById("a13").style.color = warna;
+    document.getElementById("a14").style.color = warna;
+    document.getElementById("a15").style.color = warna;
+    document.getElementById("a16").style.color = warna;
+    document.getElementById("a17").style.color = warna;
+    document.getElementById("a18").style.color = warna;
+    
+}
+
+document.getElementById("submitColor2").addEventListener("click", changeBackground1, false);
+
 
 function enablemouse(event) {
     alert("You pressed button: " + event.button);
@@ -18,3 +40,22 @@ function enablemouse(event) {
 
 document.getElementById("enablemouse").addEventListener("click", function (){
 document.getElementById("test").innerHTML = "Hello World";});
+
+var event_object = document.getElementsByTagName("html");
+
+function emitKeyEvent() {
+
+	//    This function triggers the event
+    var keyEvent = new KeyboardEvent("keypress", {key : "p", char : "p", ctrlKey: true, shiftKey: true});
+    event_object.dispatchEvent(keyEvent);
+}
+
+event_object.addEventListener('keypress', function(e){
+    
+    //    This function listens to the event
+    console.log(event, 'ctrlKey: ' + e.ctrlKey, 'shiftKey: '+ e.shiftKey, e.char, e.key);
+    alert('CTRL + SHIFT + p pressed')
+    
+    e.preventDefault();
+
+});
